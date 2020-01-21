5e8fcfc7296356cd03155c50809ce62958aa0ad6
bogdan <bogdan@LAPTOP-IM9FMRJR>
2020-01-21 20:28:33 +0100

    hola

---
+++ src/package2/Archivo2.java
@@ -0,0 +1,9 @@
+package package2;
+
+public class Archivo2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("");
+	}
+
+}
---
+++ src/package3/Archivo3.java
@@ -0,0 +1,9 @@
+package package3;
+
+public class Archivo3 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("");
+	}
+
+}
